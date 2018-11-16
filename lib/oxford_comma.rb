322a1b70cf4cec3cf48cb7f27440a9781c26b756
shinik.d.cupo <shinik.d.cupo@gmail.com>
2018-11-16 00:01:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  if (array.size == 1)
+    return array[0]
+  elsif (array.size == 2)
+    return array.join(" and ")
+  elsif (array.size == 3)
+    return (array[0] + ", " + array[1] + ", and " + array[2])
+  else
+    str = ""
+    i = 0
 
+    while i < array.size - 1
+      str << "#{array[i]}, "
+      i += 1
+    end
+    str << "and #{array[i]}"
+
+    return str
+  end
 end
